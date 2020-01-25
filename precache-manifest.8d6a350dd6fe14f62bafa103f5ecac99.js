3638e6e06a6bd14d9d9d315586fb5d794e94a3c8
iswahyudi <iswahyudi1006@gmail.com>
2020-01-25 10:45:08 +0700

    deploy

---
+++ precache-manifest.8d6a350dd6fe14f62bafa103f5ecac99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6850aadfb95dab39658784e7d3a0bd55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "090f5520a1e74148ce58",
+    "url": "/static/css/main.7fbd5a3c.chunk.css"
+  },
+  {
+    "revision": "98a60fe867bf922ae4ec",
+    "url": "/static/js/2.e13bb007.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.e13bb007.chunk.js.LICENSE"
+  },
+  {
+    "revision": "090f5520a1e74148ce58",
+    "url": "/static/js/main.1e5c8ebd.chunk.js"
+  },
+  {
+    "revision": "295cf8f3856b84559d3d",
+    "url": "/static/js/runtime-main.4ef231fe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
